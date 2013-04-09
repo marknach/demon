3f923943089d0c23c929864a1e325fff2bfc542a
Colin Gintz <gintz@Euphony.(none)>
2013-04-09 01:03:53 -0400

    Sphere collision now working. array float * r added to account for sphere radii. Added radii into example file. collision.h deprecated, removed. Example file renamed to input.txt from derp.txt.

--- vector_calc.c
+++ vector_calc.c
@@ -14,7 +14,6 @@ void calculateNextPositions()
 		fprintf(output_file,"%lf,%lf,",p[i][0],p[i][1]);
 		p[i][0] += time_step * v[i][0];
 		p[i][1] += time_step * v[i][1];
-		
 	}
 	
 	int i = number_of_vectors - 1;
@@ -24,7 +23,7 @@ void calculateNextPositions()
 	
 	fprintf(output_file,"\n");
 	
-	
+	detect_ball_collision();
 	detect_wall_collision();
 }
 
@@ -57,6 +56,7 @@ void initialize_simulation_memory(char *input_filename, char *output_filename)
 	p = (float **)malloc(number_of_vectors * sizeof(float *));
     v = (float **)malloc(number_of_vectors * sizeof(float *));
     a = (float **)malloc(number_of_vectors * sizeof(float *));
+    r = (float *)malloc(number_of_vectors * sizeof(float *));
 	
 	fclose(input_file);
 	
@@ -105,6 +105,8 @@ void initialize_simulation_memory(char *input_filename, char *output_filename)
 		
 		fscanf(input_file,"%f",&a[i][0]);
 		fscanf(input_file,"%f",&a[i][1]);
+		
+		fscanf(input_file,"%f",&r[i]);
     }
 	fclose(input_file);
 }
@@ -119,12 +121,12 @@ void detect_wall_collision()
 		//reverse
 		if(p[i][0] <= lower_boundary_x)
 		{
-			v[i][0] = -1 * v[i][1];
+			v[i][0] = -1 * v[i][0];
 			p[i][0] = lower_boundary_x + 0.00001f; //hacky way of making sure the balls don't leave the boundaries
 		}
 		else if(p[i][0] >= upper_boundary_x)
 		{
-			v[i][0] = -1 * v[i][1];
+			v[i][0] = -1 * v[i][0];
 			p[i][0] = upper_boundary_x - 0.00001f; //hacky way of making sure the balls don't leave the boundaries
 		}
 		//hits horizontal wall
@@ -137,6 +139,46 @@ void detect_wall_collision()
 		{
 			v[i][1] = -1 * v[i][1];
 			p[i][1] = upper_boundary_y - 0.00001f; //hacky way of making sure the balls don't leave the boundaries
+		}
+	}
+}
+
+void detect_ball_collision()
+{
+	//Given the set of Balls in an array
+	int i, j;	
+	for(i = 0; i < number_of_vectors - 1; i++)
+	{
+		for(j = i+1; j < number_of_vectors; j++)
+		{
+			float collision[2];
+			collision[0] = p[i][0] - p[j][0];
+			collision[1] = p[i][1] - p[j][1];
+			float D = sqrt(((p[i][0] - p[j][0]) * (p[i][0] - p[j][0])) + ((p[i][1] - p[j][1]) * (p[i][1] - p[j][1])));
+			
+			if(D == 0.0f)
+			{
+				collision[0] = 1;
+				collision[1] = 0;
+				D = 1.0f;
+			}
+			printf("D = %lf\n", D);
+			if(D < r[i] + r[j])
+			{
+				printf("HRNK\n");
+				collision[0] /= D;
+				collision[1] /= D;
+				float aci = v[i][0] * collision[0] + v[i][1] * collision[1]; 
+				float bci = v[j][0] * collision[0] + v[j][1] * collision[1];
+
+				float acf = bci;
+				float bcf = aci;
+
+				v[i][0] += (acf - aci) * collision[0];
+				v[i][1] += (acf - aci) * collision[1];
+				v[j][0] += (bcf - bci) * collision[0];
+				v[j][1] += (bcf - bci) * collision[1];
+			}
 		}
 	}
 }
